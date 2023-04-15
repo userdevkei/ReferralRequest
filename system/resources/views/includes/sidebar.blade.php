1f6cb5592650c49c29a3ecce4e076994d537d192
ernestnash <eernestnnashon@gmail.com>
2023-04-15 19:59:12 +0300

    Dashboard modifications

--- system/resources/views/includes/sidebar.blade.php
+++ system/resources/views/includes/sidebar.blade.php
@@ -12,44 +12,9 @@
 
         <li class="nav-item">
             <a class="nav-link collapsed" data-bs-target="#tables-nav" data-bs-toggle="collapse" href="#">
-                <i class="bi bi-layout-text-window-reverse"></i><span>Patients</span><i class="bi bi-chevron-down ms-auto"></i>
-            </a>
-            <ul id="tables-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-                <li>
-                    <a href="{{ route('referrals.index') }}">
-                        <i class="bi bi-circle"></i><span>Incoming Referrals</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="{{ route('referral.outgoing') }}">
-                        <i class="bi bi-circle"></i><span>Outgoing Referrals</span>
-                    </a>
-                </li>
-            </ul>
-        </li><!-- End Tables Nav -->
-        <li class="nav-item">
-            <a class="nav-link collapsed" data-bs-target="#tables-nav2" data-bs-toggle="collapse" href="#">
                 <i class="bi bi-layout-text-window-reverse"></i><span>Referrals</span><i class="bi bi-chevron-down ms-auto"></i>
             </a>
-            <ul id="tables-nav2" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-                <li>
-                    <a href="{{ route('referrals.index') }}">
-                        <i class="bi bi-circle"></i><span>Incoming Referrals</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="{{ route('referral.outgoing') }}">
-                        <i class="bi bi-circle"></i><span>Outgoing Referrals</span>
-                    </a>
-                </li>
-            </ul>
-        </li><!-- End Tables Nav -->
-
-        <li class="nav-item">
-            <a class="nav-link collapsed" data-bs-target="#tables-nav1" data-bs-toggle="collapse" href="#">
-                <i class="bi bi-layout-text-window-reverse"></i><span>Facilty</span><i class="bi bi-chevron-down ms-auto"></i>
-            </a>
-            <ul id="tables-nav1" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+            <ul id="tables-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
                 <li>
                     <a href="{{ route('referrals.index') }}">
                         <i class="bi bi-circle"></i><span>Incoming Referrals</span>
